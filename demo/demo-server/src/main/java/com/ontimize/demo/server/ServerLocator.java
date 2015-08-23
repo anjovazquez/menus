13618b651bc1b6731da766e142101353897ddf34
anjo <anjo@anjo-notebook>
2015-08-23 17:16:46 +0200

    Product entity implementation!

--- demo/demo-server/src/main/java/com/ontimize/demo/server/ServerLocator.java
+++ demo/demo-server/src/main/java/com/ontimize/demo/server/ServerLocator.java
@@ -1,11 +1,78 @@
 package com.ontimize.demo.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Hashtable;
+
 import com.ontimize.locator.SecureReferenceLocator;
+import com.ontimize.util.remote.BytesBlock;
 
 public class ServerLocator extends SecureReferenceLocator {
+	
+	private String imagesFilePath;
 
 	public ServerLocator(int port, Hashtable parameters) throws Exception {
 		super(port, parameters);
+		imagesFilePath = (String) parameters.get("ImagesFilePath");
 	}
+	
+	public void saveImage(String imageId, BytesBlock image) throws Exception {
+		File imgDir = new File(imagesFilePath);
+		if(imgDir.exists()){
+			String pathFileImage = imagesFilePath+File.separator+imageId;
+			System.out.println(pathFileImage);
+			File f = new File(pathFileImage);
+			if(!f.exists())
+				f.createNewFile();
+			FileOutputStream out = new FileOutputStream(f, false);
+			out.write(image.getBytes());
+			out.flush();
+		}
+	}
+	
+	public byte[] read(File file) throws IOException {
+//	    if (file.length() > MAX_FILE_SIZE) {
+//	        throw new FileTooBigException(file);
+//	    }
+	    ByteArrayOutputStream ous = null;
+	    InputStream ios = null;
+	    try {
+	        byte[] buffer = new byte[4096];
+	        ous = new ByteArrayOutputStream();
+	        ios = new FileInputStream(file);
+	        int read = 0;
+	        while ((read = ios.read(buffer)) != -1) {
+	            ous.write(buffer, 0, read);
+	        }
+	    }finally {
+	        try {
+	            if (ous != null)
+	                ous.close();
+	        } catch (IOException e) {
+	        }
+	        try {
+	            if (ios != null)
+	                ios.close();
+	        } catch (IOException e) {
+	        }
+	    }
+	    return ous.toByteArray();
+	}
+	
+	public BytesBlock getImage(String imageId) throws Exception {
+		File imgPath = new File(imagesFilePath+File.separator+imageId);
+		if(imgPath.exists()){
+			BytesBlock bytesBlock = new BytesBlock(read(imgPath));
+			return bytesBlock;
+		}
+//		throw new ImageNotFoundException(imageId);
+		throw new Exception();
+	}
+	
+	
+	
 }
--- demo/demo-server/src/main/java/com/ontimize/demo/server/entities/EProduct.java
+++ demo/demo-server/src/main/java/com/ontimize/demo/server/entities/EProduct.java
@@ -3,11 +3,14 @@ package com.ontimize.demo.server.entities;
 import java.sql.Connection;
 import java.util.Hashtable;
 import java.util.Properties;
+import java.util.Vector;
 
 import com.ontimize.db.DatabaseConnectionManager;
 import com.ontimize.db.EntityResult;
 import com.ontimize.db.TableEntity;
+import com.ontimize.demo.server.ServerLocator;
 import com.ontimize.locator.EntityReferenceLocator;
+import com.ontimize.util.remote.BytesBlock;
 
 public class EProduct extends TableEntity {
 
@@ -22,11 +25,44 @@ public class EProduct extends TableEntity {
 	}
 
 	@Override
-	public EntityResult insert(Hashtable arg0, int arg1, Connection arg2) throws Exception {
-		// TODO Auto-generated method stub
-		return super.insert(arg0, arg1, arg2);
+	public EntityResult insert(Hashtable keyValues, int sessionId, Connection con) throws Exception {
+		if (keyValues.containsKey("productImageName") && keyValues.containsKey("productImage")) {
+			try {
+				((ServerLocator) locator).saveImage((String) keyValues.get("productImageName"),
+						(BytesBlock) keyValues.remove("productImage"));
+			} catch (Exception e) {
+				e.printStackTrace();
+//				throw new Exception();
+			}
+		}
+		return super.insert(keyValues, sessionId, con);
+
 	}
-	
-	
+
+	@Override
+	public EntityResult query(Hashtable keysValues, Vector attributes, int sessionId, Connection con) throws Exception {
+
+		EntityResult result = super.query(keysValues, attributes, sessionId, con);
+
+		EntityResult res = new EntityResult(getColumns());
+		for (int i = 0; i < result.calculateRecordNumber(); i++) {
+			Hashtable valProd = result.getRecordValues(i);
+			String prodImg = (String) valProd.get("productImageName");
+
+			if (prodImg != null) {
+				try {
+					BytesBlock image = ((ServerLocator) locator).getImage(prodImg);
+					valProd.put("productImage", image);
+				} catch (Exception e) {
+					e.printStackTrace();
+//					throw new Exception();
+				}
+			}
+
+			res.addRecord(valProd);
+		}
+
+		return res;
+	}
 
 }
