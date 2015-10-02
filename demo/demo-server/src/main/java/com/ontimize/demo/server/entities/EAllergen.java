4fdd04213080323e48850cfb728ae3bef6bb6be6
anjo <anjo@anjo-notebook>
2015-10-02 23:33:31 +0200

    Ingredients & Allergens included

---
+++ demo/demo-server/src/main/java/com/ontimize/demo/server/entities/EAllergen.java
@@ -0,0 +1,27 @@
+package com.ontimize.demo.server.entities;
+
+import java.sql.Connection;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Vector;
+
+import com.ontimize.db.DatabaseConnectionManager;
+import com.ontimize.db.EntityResult;
+import com.ontimize.db.TableEntity;
+import com.ontimize.demo.server.ServerLocator;
+import com.ontimize.locator.EntityReferenceLocator;
+import com.ontimize.util.remote.BytesBlock;
+
+public class EAllergen extends TableEntity {
+
+	public EAllergen(EntityReferenceLocator locator, DatabaseConnectionManager dbConnectionManager, int port)
+			throws Exception {
+		super(locator, dbConnectionManager, port);
+	}
+
+	public EAllergen(EntityReferenceLocator locator, DatabaseConnectionManager dbConnectionManager, int port,
+			Properties prop, Properties aliasProp) throws Exception {
+		super(locator, dbConnectionManager, port, prop, aliasProp);
+	}
+
+}
